bb99765e7a5367c2919f93813ceb697a4731beeb
chanleyou <chanleyou@gmail.com>
2023-10-03 13:30:55 +0800

    [STA-5] lock pragma

--- src/lib/SafeTransferLib.sol
+++ src/lib/SafeTransferLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.13;
+pragma solidity 0.8.13;
 
 /// @notice Gas-efficient safe ETH transfer library that gracefully handles missing return values.
 /// @author Copied from Solmate with ERC20 code removed.
